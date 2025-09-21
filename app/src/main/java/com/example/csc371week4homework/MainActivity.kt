59f7d0ff173d6c7f2e551fde1715febff562bc74
Jason Devaraj <jdevaraj01@gmail.com>
2025-09-21 19:00:35 -0400

    Add main logic.

--- app/src/main/java/com/example/csc371week4homework/MainActivity.kt
+++ app/src/main/java/com/example/csc371week4homework/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.csc371week4homework
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            calculateGPA()
+        }
+    }
+
+    fun calculateGPA() {
+        val gpa1 : Double = findViewById<EditText>(R.id.editTextNumberDecimal).text.toString().toDouble()
+        val gpa2 : Double = findViewById<EditText>(R.id.editTextNumberDecimal2).text.toString().toDouble()
+        val gpa3 : Double = findViewById<EditText>(R.id.editTextNumberDecimal3).text.toString().toDouble()
+        val gpa4 : Double = findViewById<EditText>(R.id.editTextNumberDecimal4).text.toString().toDouble()
+        val gpa5 : Double = findViewById<EditText>(R.id.editTextNumberDecimal5).text.toString().toDouble()
+
+        val computedGPA : Double = (gpa1 + gpa2  + gpa3 + gpa4 + gpa5) / 5.0
+
+        val output = findViewById<TextView>(R.id.textView5)
+        output.text = computedGPA.toString()
+
     }
 }
